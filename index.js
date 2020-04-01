7e71eb0c69267a2962b6d3e3f49f33594994edab
Glitch (baran-utube-altyap-) <none>
2020-04-01 16:39:51 +0000

    🐌🏣 Checkpoint ./index.js:11195635/730

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-    app.get("!", (request, response) => {
-    console.log(`Bot hazıır.`);
+    app.get("/", (request, response) => {
+    console.log(` az önce pinglenmedi. Sonra ponglanmadı... ya da başka bir şeyler olmadı.`);
     response.sendStatus(200);
     });
     app.listen(process.env.PORT);
     setInterval(() => {
-    http.get(`https://baran-utube-altyap-.glitch.me`);
+    http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
     }, 280000);
 
 const Discord = require('discord.js');
