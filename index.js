46bbf75d7616f951a8060c6dd647c14bd884cf51
Glitch (baran-utube-altyap-) <none>
2020-04-02 11:45:08 +0000

    🎹🙅 Checkpoint ./index.js:11195635/331

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-    app.get("!", (request, response) => {
-    console.log(`Bot hazır.`);
-    response.sendStatus(200);
-    });
-    app.listen(process.env.PORT);
-    setInterval(() => {
-    http.get(`https://baran-utube-altyap-.glitch.me/`);
-    }, 280000);
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const ayarlar = require('./ayarlar.json');
